228a1b1719d7ab52d3be7739525dce759df266c6
johnalmajan <johnalmajan@localhost>
2020-03-13 13:05:34 -0700

    1:05 PM 3.13.20

--- src/Sports/basketballPlayer.java
+++ src/Sports/basketballPlayer.java
@@ -3,6 +3,15 @@ package Sports;
 public class basketballPlayer extends player{
 	private int numWins;
 	private double shoeSize;
-	private String Sponsor;
-
+	private String sponsor;
+	
+	public basketballPlayer(int height, String h, String n, int nW, double sS, String s) {
+		super(height, h, n);
+		numWins = nW;
+		shoeSize = sS;
+		sponsor = s;
+	}
+	public String toString() {
+		return super.toString() + " | number of wins: " + numWins + " | shoe size = " + shoeSize + " | sponsor = " + sponsor;
+	}
 }
--- src/Sports/soccerPlayer.java
+++ src/Sports/soccerPlayer.java
@@ -1,5 +1,17 @@
 package Sports;
 
 public class soccerPlayer extends player{
-
+	private String position;
+	private int number;
+	private int goalsScored;
+	
+	public soccerPlayer(int height, String h, String n, String p, int num, int gS) {
+		super(height, h, n);
+		position = p;
+		number = num;
+		goalsScored = gS;
+	}
+	public String toString() {
+		return super.toString() + " | field position: " + position + " | player number = " + number + " | goals scored = " + goalsScored;
+	}
 }
